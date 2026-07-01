8e1d9e4636612422c855215aee62ca7967677147
mjjeff <akanamajeffrey@gmail.com>
2026-07-01 16:07:43 +0100

    first commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,38 @@
-using System;
+Random random = new Random();
 
-class Program
+string playAgain;
+
+do
 {
-    static void Main(string[] args)
+    int magicNumber = random.Next(1, 101);
+
+    int guess = -1;
+    int guesses = 0;
+
+    while (guess != magicNumber)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+
+        guesses++;
+
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
-}
+
+    Console.WriteLine($"You guessed it in {guesses} tries.");
+
+    Console.Write("Do you want to play again? (yes/no): ");
+    playAgain = Console.ReadLine().ToLower();
+
+} while (playAgain == "yes");
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,42 @@
-using System;
+using System.Collections.Generic;
 
-class Program
+List<int> numbers = new List<int>();
+
+Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+int number;
+
+do
 {
-    static void Main(string[] args)
+    Console.Write("Enter number: ");
+    number = int.Parse(Console.ReadLine());
+
+    if (number != 0)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        numbers.Add(number);
+    }
+
+} while (number != 0);
+
+int sum = 0;
+
+foreach (int value in numbers)
+{
+    sum += value;
+}
+
+double average = (double)sum / numbers.Count;
+
+int largest = numbers[0];
+
+foreach (int value in numbers)
+{
+    if (value > largest)
+    {
+        largest = value;
     }
 }
+
+Console.WriteLine($"The sum is: {sum}");
+Console.WriteLine($"The average is: {average}");
+Console.WriteLine($"The largest number is: {largest}");
